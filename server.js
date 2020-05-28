c33ed50482c6b640feb96a5b8d39d931c522cbea
taha uygun <tahauygun@gmail.com>
2020-05-27 23:19:17 -0700

    adding cluster fork

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const cluster = require("cluster");
 const totalCPUs = requires('os').cpus().length;
+
+const fibObj = require("./math-logic/fibonacci-series");
+
+if(cluster.isMaster){
+	console.log(`Total number of CPU counts is ${totalCPUs}`);
+	for (var i = 0; i<totalCPUs; i++){
+		cluster.fork();
+	}
+	
+	cluster("online", worker=>{
+		console.log(`Worker ID ${worker.id} and PID is ${worker.process.pid}`);
+	});
+	cluster.on("exit", worker=> {
+		console.log(`Worker ID ${worker.id} and PID is ${worker.process.pid} is offline`);
+		console.log(`Lets fork new worker!");
+		cluster.fork();
+	});
+}
