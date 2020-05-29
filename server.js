6e8f70875784ceefc8232053bdf8e3e5ba253afd
taha uygun <tahauygun@gmail.com>
2020-05-28 23:30:14 -0700

    some fixes

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cluster = require("cluster");
-const totalCPUs = requires('os').cpus().length;
+const totalCPUs = require('os').cpus().length;
 
-const fibObj = require("./math-logic/fibonacci-series");
+const fabObj = require("./math-logic/fibonacci-series");
+if (cluster.isMaster) {
 
-if(cluster.isMaster){
-	console.log(`Total number of CPU counts is ${totalCPUs}`);
-	for (var i = 0; i<totalCPUs; i++){
+	console.log(`Total Number of CPU Counts is ${totalCPUs}`);
+
+	for (var i = 0; i < totalCPUs; i++) {
 		cluster.fork();
 	}
-	
-	cluster("online", worker=>{
-		console.log(`Worker ID ${worker.id} and PID is ${worker.process.pid}`);
+	cluster.on("online", worker => {
+		console.log(`Worker Id is ${worker.id} and PID is ${worker.process.pid}`);
 	});
-	cluster.on("exit", worker=> {
-		console.log(`Worker ID ${worker.id} and PID is ${worker.process.pid} is offline`);
-		console.log(`Lets fork new worker!");
+	cluster.on("exit", worker => {
+		console.log(`Worker Id ${worker.id} and PID is ${worker.process.pid} is offline`);
+		console.log("Let's fork new worker!");
 		cluster.fork();
 	});
-} else {
-	const app = express();
-	app.get("/", (req, res) => {
-		console.log(`Worker Process ID - ${cluster.worker.process.pid} has accepted the request!`);
-		let number = fabObj.calculateFibonacciValue(Number.parseInt(req.query.number));
-		res.send(`<h1>${number}</h1>`);
-	});
-	
-	app.listen(3000, () => console.log("Express App is running on PORT : 3000"));
 }
+else {
+	const app = express();
+	app.get("/", (request, response) => {
+		console.log(`Worker Process Id - ${cluster.worker.process.pid} has accepted the request!`);
+		let number = fabObj.calculateFibonacciValue(Number.parseInt(request.query.number));
+		response.send(`<h1>${number}</h1>`);
+	});
+
+	app.listen(3000, () => console.log("Express App is running on PORT : 3000"));
